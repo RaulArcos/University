f9f2c80bd49cc3d55a118928f17cf06bd4e234ed
RaulArcos <1.raularcos@gmail.com>
2022-02-24 20:45:16 +0100

    práctica 1 completa funcional

--- EDNL/P1/p1.cpp
+++ EDNL/P1/p1.cpp
@@ -4,8 +4,6 @@ Práctica 1 EDNL by Raúl Arcos Herrera
 
 #include <iostream>
 #include <fstream>
-#include "abin.h"
-#include "abin_E-S.h"
 #include "p1.h"
 using namespace std;
 typedef char tElto;
@@ -22,11 +20,11 @@ Abin<tElto> A;
  fs.close();
  cout << "\n*** Árbol A guardado en fichero abin.dat ***\n";
  cout << "\n*** Lectura de árbol binario B de abin.dat ***\n";
- */
-
+ 
+*/
 
 //IMPRIME EL ARBOL B A PARTIR DEL FICHERO ABIN.DAT
-ifstream fe("abin.dat"); // Abrir fichero de entrada.
+ifstream fe("../abin.dat"); // Abrir fichero de entrada.
 rellenarAbin(fe, A); // Desde fichero.
 fe.close();
 cout << "\n*** Mostrar árbol binario B ***\n";
@@ -41,5 +39,8 @@ cout << "(Representacion enlazada) El nodo 'k' tiene altura: " << A.alturaNodoRe
 cout << "(Representacion enlazada) El nodo 'k' tiene profundidad: " << A.profundidadNodoRec(A.hijoDrcho(A.hijoDrcho(A.hijoDrcho(A.raiz())))) << "." <<endl;
 cout << "El arbol tiene un desequilibrio de: " << desequilibrioAbin(A) << "." <<endl;
 
-
+if(pseudocompleto(A))
+    cout << "El arbol ES pseudocompleto." << endl;
+else
+    cout << "El arbol NO ES pseudocompleto." << endl;
 } 
--- EDNL/P1/p1.h
+++ EDNL/P1/p1.h
@@ -5,8 +5,8 @@ Práctica 1 EDNL by Raúl Arcos Herrera
 #ifndef P1_H
 #define P1_H
 
-#include "abin.h"
-#include "abin_E-S.h"
+#include "../abin.h"
+#include "../abin_E-S.h"
 #include <stdlib.h>
 
 /*
@@ -58,21 +58,44 @@ definido como el máximo desequilibrio de todos sus nodos. El desequilibrio de u
 define como la diferencia entre las alturas de los subárboles del mismo. 
 */
 template <typename T>
-int desequilibrioNodo(const Abin<T>& A, const typename Abin<T>::nodo n){
-    return A.alturaNodoRec(A.hijoIzqdo(n)) - A.alturaNodoRec(A.hijoDrcho(n));
+int desequilibrioNodo(Abin<T>& A, const typename Abin<T>::nodo n){
+    return std::abs(A.alturaNodoRec(A.hijoIzqdo(n)) - A.alturaNodoRec(A.hijoDrcho(n)));
 }
 
 template <typename T>
-int desequilibrioAbin(const Abin<T>&A){
+int desequilibrioAbin(Abin<T>&A){
     return desequilibrioAbinRec(A,A.raiz());
 }
 
 template <typename T>
-int desequilibrioAbinRec(const Abin<T>&A, const typename Abin<T>::nodo n){
+int desequilibrioAbinRec(Abin<T>&A, const typename Abin<T>::nodo n){
     if(n == Abin<T>::NODO_NULO)
         return 0;
     else 
         return std::max(desequilibrioNodo(A,n), std::max(desequilibrioAbinRec(A,A.hijoIzqdo(n)),desequilibrioAbinRec(A,A.hijoDrcho(n))));
 }
 
+/*
+7.Implementa un subprograma que determine si un árbol binario es o no pseudocompleto.
+En este problema entenderemos que un árbol es pseudocompleto, si en el penúltimo nivel
+del mismo cada uno de los nodos tiene dos hijos o ninguno
+*/
+template <typename T>
+bool pseudocompleto( Abin<T>& A){
+    return pseudocompletoRec(A,A.raiz());
+}
+template <typename T>
+bool pseudocompletoRec( Abin<T>&A, const typename Abin<T>::nodo n){
+    if(n == Abin<T>::NODO_NULO)
+        return false;
+    else{
+        if(A.alturaNodoRec(n) == alturaAbin(A) - 1)
+            if(A.hijoIzqdo(n) == Abin<T>::NODO_NULO && A.hijoDrcho(n) == Abin<T>::NODO_NULO || A.hijoIzqdo(n) != Abin<T>::NODO_NULO && A.hijoDrcho(n) != Abin<T>::NODO_NULO)
+                return true;
+            else 
+                return false;
+        else 
+            return pseudocompletoRec(A,A.hijoIzqdo(n)) && pseudocompletoRec(A,A.hijoDrcho(n));
+    }
+}
 #endif 
