64fe628e0861e87125d65449434955824f003f0f
Raúl Arcos Herrera <raul.arcosherrera@alum.uca.es>
2022-10-26 15:50:09 +0200

    archivos p2 sin procesar

---
+++ PCTR/21-22/Práctica 2/Usa_hebra.java
@@ -0,0 +1,14 @@
+/**
+ * Usa_hebra.java llama al método ejecutar de la clase hebra.
+ * @author Raúl Arcos Herrera
+ * @version 2022
+ */
+import java.util.Scanner;
+
+public class Usa_hebra {
+    public static void main( String[] args){
+        try{
+           System.out.println(hebra.ejecutar()); 
+        }catch (Exception ex){}
+    }
+}
